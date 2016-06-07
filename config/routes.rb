e85bac6f18fedc558394e81e4bec041979fc75e8
濱口魁希 <k.hamaguchi15@gmail.com>
2016-06-07 23:18:24 +0900

    サインイン / サインアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
 
   root 'prototypes#index'
 
-  get  '/prototypes/index'  =>    'prototypes#index'
-  get  '/prototypes/new'    =>    'prototypes#new'
-  get  '/prototypes/show'   =>    'prototypes#show'
+  resources :prototypes
 end
