11ccb1388d4f41e4adf6d8fa66982c6f7e93b70e
濱口魁希 <k.hamaguchi15@gmail.com>
2016-08-03 08:59:33 +0900

    aタグをlink_toに変換

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626114845) do
+ActiveRecord::Schema.define(version: 20160704020334) do
 
   create_table "projects", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "catch_copy",  limit: 65535
-    t.text     "concept",     limit: 65535
+    t.string   "title",      limit: 255
+    t.text     "catch_copy", limit: 65535
+    t.text     "concept",    limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id_id",  limit: 4
-    t.integer  "likes_id_id", limit: 4
+    t.integer  "user_id",    limit: 4
+    t.integer  "likes_id",   limit: 4
+  end
+
+  create_table "thumbnails", force: :cascade do |t|
+    t.text     "image_url",  limit: 65535
+    t.integer  "status",     limit: 4
+    t.integer  "project_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
