2cfcbd16400f76d64a8c897a63c28a11b054b6a1
濱口魁希 <k.hamaguchi15@gmail.com>
2016-08-03 19:28:07 +0900

    編集機能

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -24,14 +24,27 @@ class ProjectsController < ApplicationController
     redirect_to projects_path, notice: 'Prototype was successfully deleted.'
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    project = Project.find(params[:id])
+    if project.user_id == current_user.id
+       project.update(params_project)
+    end
+    redirect_to projects_path, notice: 'Prototype was successfully updated.'
+  end
+
   private
   def params_project
     params.require(:project).permit(
       :title,
       :catch_copy,
       :concept,
-      thumbnails_attributes:[:image_url, :status]
+      thumbnails_attributes:[:id, :image_url, :status]
       ).merge(user_id: current_user.id)
   end
+
 
 end
