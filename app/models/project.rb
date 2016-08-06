6778a2a4eb45a1f8fbfe07b6a5df207ff8c64218
濱口魁希 <k.hamaguchi15@gmail.com>
2016-08-07 01:04:25 +0900

    comment機能

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
-  belongs_to   :user
-    has_many   :thumbnails
-    has_many   :comments
+    belongs_to :user
+    has_many :thumbnails
+    has_many :comments
     accepts_nested_attributes_for :thumbnails
 
 end
