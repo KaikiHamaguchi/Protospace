79b3a3e62444af3157d777251f71466abed37f21
濱口魁希 <k.hamaguchi15@gmail.com>
2016-08-13 01:52:00 +0900

    newestとpopular機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'projects#index'
   resources :users, only: [:show, :edit, :update]
+  resources :popular
+  resources :newest
   resources :projects do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
