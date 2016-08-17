57a7739b281f18eb5fce47a6937a3886d229104c
濱口魁希 <k.hamaguchi15@gmail.com>
2016-08-17 17:16:41 +0900

    タグ機能追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
-  class Project < ActiveRecord::Base
+class Project < ActiveRecord::Base
     belongs_to :user
     has_many :thumbnails
     has_many :comments
@@ -8,6 +8,8 @@
     validates :title, presence: true
     validates :catch_copy, presence: true
     validates :concept, presence: true
+
+    acts_as_taggable
 
   def like_user(user)
    likes.find_by(user_id: user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update]
   resources :popular, only: [:index]
   resources :newest, only: [:index]
+  resources :tags, only: [:index, :show]
   resources :projects do
     resources :comments, only: [:create]
     resources :likes, only: [:create, :destroy]
