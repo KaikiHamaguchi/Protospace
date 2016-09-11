7197b9e96e3b33ee28c1459bb31c37731d778d0a
濱口魁希 <k.hamaguchi15@gmail.com>
2016-09-11 21:07:17 +0900

    1.projectが削除された際にcommentも削除されるようにdependent: :destroyを記述。 2.like_userメソッドの引数が間違えていたので訂正。

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
     belongs_to :user
     has_many :thumbnails
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
     accepts_nested_attributes_for :thumbnails
 
@@ -11,7 +11,7 @@ class Project < ActiveRecord::Base
 
     acts_as_taggable
 
-  def like_user(user)
-   likes.find_by(user_id: user_id)
+  def like_user(user_id)
+   likes.exists?(user_id: user_id)
   end
 end
