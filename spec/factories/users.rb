a46bdf33c8e61da5b579e1c305b33fade39f3600
濱口魁希 <k.hamaguchi15@gmail.com>
2016-09-15 19:49:00 +0900

    emailをダミープログラムにした。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :user do
     name                  "kaiki"
-    email                 "kkk@gmail.com"
+    email { Faker::Internet.email }
     password              "00000000"
     password_confirmation "00000000"
   end
