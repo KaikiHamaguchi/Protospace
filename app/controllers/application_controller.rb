4d7c55c8ddcb4a18a0484b1ae282c39637581491
濱口魁希 <k.hamaguchi15@gmail.com>
2016-09-15 19:45:07 +0900

    新規登録時とレコード更新時にStrongParametersを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:name, :profile_image, :member, :profile, :work)
-    devise_parameter_sanitizer.for(:account_update).push(:name, :profile_image, :member, :profile, :work)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile_image, :member, :profile, :work])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image, :member, :profile, :work])
   end
+
 end
